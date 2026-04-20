61f44eafa1b0bfb6c36fb873a8d5ad9f6774b6f8
Victor Fernandes <victorfariafernandes@gmail.com>
2026-04-20 03:34:10 -0300

    feat: refactor portrait image source to use imported variable

--- src/lib/components/organisms/section/AboutSection.svelte
+++ src/lib/components/organisms/section/AboutSection.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { t } from '$lib/stores/locale.store';
 	import { SectionTitle } from '$lib/components/molecules';
+	  import portrait from '$lib/assets/portrait.png';
+
 </script>
 
 <section id="about-me" class="mx-auto flex max-w-7xl flex-col gap-12 px-container py-section md:flex-row md:items-center">
@@ -9,7 +11,7 @@
 	<div class="w-full shrink-0 md:w-95">
 		<div class="relative pb-3 pr-3">
 			<img
-				src="src/lib/assets/portrait.png"
+				src={portrait}
 				alt={$t.pages.about.name}
 				onerror={(e) => { (e.currentTarget as HTMLImageElement).style.opacity = '0'; }}
 				class="relative z-10 block aspect-3/4 w-full object-cover object-top"
